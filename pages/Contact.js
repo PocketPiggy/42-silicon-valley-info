3a8803315bf52aa9d1f46b61a1300f71842b396a
pocketpiggy <pocketpiggy@protonmail.com>
2020-10-07 16:19:36 -0700

    added emailjs to receive emails. commiting early to test if component is functional.

--- pages/Contact.js
+++ pages/Contact.js
@@ -1,33 +1,51 @@
 import React from "react";
+import emailjs from "emailjs-com";
 
-export default ({ content }) => (
-  <div>
-    <h1>{content.title}</h1>
-    <p>As a test, does this show up in source?</p>
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <p> Yes, I bought your colgate toothpaste. </p>
-    <p> The one with Tartar control. </p>
-    <p>{content.p1}</p>
-  </div>
-);
+export default function Contact({ content }) {
+  const sendEmailToSelf = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('gmail', 'template_BM69uuKG', e.target, process.env.USER_ID)
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    e.target.reset();
+  }
+
+  return (
+    <div>
+
+      <h1>{content.title}</h1>
+      <p>
+        {" "}
+        Any questions? Concerns? Inquiries? Bugs on the website? Send me an
+        email.
+      </p>
+      <br />
+      <br />
+      <br />
+
+      <div id='contact-form'>
+        <form className="contact-form" onSubmit={sendEmailToSelf}>
+          <input type="hidden" name="contact_number" />
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
+      </div>
+
+    </div>
+  );
+}
 
 export async function getStaticProps(content) {
-  console.log(content);
+  //console.log('ENV_VARIABLE: ', process.env.USER_ID)
   return {
     props: {
       content: {
