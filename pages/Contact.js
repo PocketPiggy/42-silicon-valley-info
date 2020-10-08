0d7402a5a008e5ad756294464e0fb66812136298
pocketpiggy <pocketpiggy@protonmail.com>
2020-10-07 20:02:57 -0700

    v0.2.0 - all components are functionally working on local/deployment.

--- pages/Contact.js
+++ pages/Contact.js
@@ -2,22 +2,9 @@ import React from "react";
 import emailjs from "emailjs-com";
 
 export default function Contact({ content }) {
-  console.log("\n BROWSER \n");
-  console.log("service_id: ", `${process.env.SERVICE_ID}`);
-  console.log("tempalte_id: ", `${process.env.TEMPLATE_ID}`);
-  console.log("user_id: ", `${process.env.USER_ID}`);
-
-  console.log("\n BROWSER - PUBLIC \n");
-  console.log("service_id: ", `${process.env.NEXT_PUBLIC_SERVICE_ID}`);
-  console.log("tempalte_id: ", `${process.env.NEXT_PUBLIC_TEMPLATE_ID}`);
-  console.log("user_id: ", `${process.env.NEXT_PUBLIC_USER_ID}`);
-
-  console.log("typeof: ", typeof process.env.NEXT_PUBLIC_USER_ID);
-
   const sendEmailToSelf = (e) => {
     e.preventDefault();
 
-    //emailjs.sendForm(`${process.env.SERVICE_ID}`, `${process.env.TEMPLATE_ID}`, e.target, `${process.env.USER_ID}`)
     emailjs .sendForm( process.env.NEXT_PUBLIC_SERVICE_ID, process.env.NEXT_PUBLIC_TEMPLATE_ID, e.target, process.env.NEXT_PUBLIC_USER_ID)
       .then(
         (result) => {
@@ -67,12 +54,7 @@ export default function Contact({ content }) {
 }
 
 export async function getStaticProps(content) {
-  //console.log('ENV_VARIABLE: ', process.env.USER_ID)
-  console.log("\n SERVER \n");
-  console.log("service_id: ", `${process.env.SERVICE_ID}`);
-  console.log("tempalte_id: ", `${process.env.TEMPLATE_ID}`);
-  console.log("user_id: ", `${process.env.USER_ID}`);
-  return {
+    return {
     props: {
       content: {
         title: "Contact",
