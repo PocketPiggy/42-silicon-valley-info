6bcc37d61cd395491b41e5c106a6110f67918d5d
pocketpiggy <pocketpiggy@protonmail.com>
2020-10-08 09:18:43 -0700

    added big heads avatar. finished about/home page content.

--- pages/About.js
+++ pages/About.js
@@ -1,117 +1,39 @@
 import React from "react";
+import { BigHead } from "@bigheads/core";
 
-export default ({ content }) => (
-  <div>
-    <h1>{content.title}</h1>
-    <p>As a test, does this show up in source?</p>
-    <br />
-    <br />
-    <br /> <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <p>{content.p1}</p>
-    <p> Made ya look! </p>
-  </div>
-);
+export default function About({ content }) {
+  const FiveHead = () => (
+    <BigHead
+      accessory="roundGlasses"
+      body="breasts"
+      circleColor="blue"
+      clothing="shirt"
+      clothingColor="black"
+      eyebrows="concerned"
+      eyes="content"
+      facialHair="none2"
+      graphic="react"
+      hair="pixie"
+      hairColor="black"
+      hat="none"
+      hatColor="black"
+      lashes
+      lipColor="pink"
+      mask
+      mouth="serious"
+      skinTone="brown"
+    />
+  );
+
+  return (
+    <div>
+      <h1>{content.title}</h1>
+      <p>{content.p1} </p>
+      <p>{content.p2} </p>
+      <FiveHead />
+    </div>
+  );
+}
 
 export async function getStaticProps(content) {
   console.log(content);
@@ -119,7 +41,10 @@ export async function getStaticProps(content) {
     props: {
       content: {
         title: "About",
-        p1: "This is the   page.",
+        p1:
+          "A brief introduction of myself: I was waiting for the April 2020 Piscine but it was cancelled in February due to 42SV needing to update 'update their techinical infrastructure.' This lead to all Piscines being cancelled for 2020 until further notice.  Then Covid-19 shortly after and the campus was shut down in March.",
+        p2:
+          "In the meantime, I've decided to continue self teaching, moving away from C and towards web development. I'm currently eagerly waiting on news for the next Piscine, which hopefully will be early next year.",
       },
     },
   };
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 
-export default ({ content }) => (
-  <div>
-    <h1>{content.title}</h1>
-    <p>As a test, does this show up in source?</p>
-    <br />
-    <br />
-    <br />
-    <p>{content.p1}</p>
-  </div>
-);
+export default function Index({ content }) {
+  return (
+    <div>
+      <h1>{content.title}</h1>
+      <p>{content.p1}</p>
+      <p>{content.p2}</p>
+      <span>
+        <img
+          src="/images/home/Marvin.jpg"
+          alt="Marvin the android, looking depressed."
+          title="DON'T PANIC!"
+        />
+      </span>
+    </div>
+  );
+}
 
 export async function getStaticProps(content) {
   console.log(content);
@@ -17,7 +23,8 @@ export async function getStaticProps(content) {
     props: {
       content: {
         title: "42 Silicon Valley information",
-        p1: "What about this one? I'm not sure, I'd like to think...no.",
+        p1: "Welcome. If you found this website, chances are you're looking for information about the school 42 Silicon Valley. This website is dedicated to putting out all pubically known information about it and how 42 itself operates. Within this website, you'll find what the Piscine is, how to prepare for it and more. If you already know about the Piscine, head on over to 'Before the Piscine'. If you're looking for more information in general, I have sourced everything I've found. You'll find interviews, articles, reviews, the curriculum and even current up to date information about 42SV.",
+        p2: "The 42 program, especially the Piscine, is very tough and requires some tenacity to get through it. Hopefully with the information I've gathered here, it'll be at least of some help for you regardless if you're on the fence about the whole thing or if you want to prepare as much as possible for the piscine. And with that out of the way, here's a quote from Marvin: 'Well I wish you'd just tell me rather than try to engage my enthusiasm.'"
       },
     },
   };
