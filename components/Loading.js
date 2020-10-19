e38288f0eff64c392a3171075838df2ddc40a9ae
pocketpiggy <pocketpiggy@protonmail.com>
2020-10-18 19:22:52 -0700

    fixed loading screen pathing

--- components/Loading.js
+++ components/Loading.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
 
-export default function Loading(){
-    return(
+export default function Loading() {
+    return (
         <div>
             <h1> Hey I'm loading! </h1>
-            <img src='/Images/loading/42Loading.jpg' />
+            <img
+                src="/Images/Components/Loading/42Loading.jpg"
+                alt='Number 42 made up of text "The answer is" repeating.'
+            />
         </div>
-    )
+    );
 }
